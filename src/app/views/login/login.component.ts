5af378b79616b47ed662b03f6f5aceaf3f30876c
martinalbrnz <martinalbor96@gmail.com>
2022-12-24 19:50:07 -0300

    Clean unused imports

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,10 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { RoutesEnum } from '@constants/routes';
@@ -15,17 +11,8 @@ import { LoginService } from '@services/login/login.service';
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatInputModule,
     MatSnackBarModule,
-    HttpClientModule,
     ReactiveFormsModule,
-  ],
-  providers: [
-    LoginService,
-    HttpClient,
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
@@ -52,18 +39,20 @@ export class LoginComponent {
   handleLogin() {
     const { password, email } = this.form.value
     this.login.login({ email: email!, password: password! })
-      .subscribe(res => {
-        if (res.error) {
-          this._snackbar.open('La contraseña o el email son incorrectos', 'Ok', {
-            verticalPosition: 'top',
-            horizontalPosition: 'right'
-          })
-        } else { // No error
-          localStorage.setItem('id', res.id)
-          this.login.setRole(res.role);
-          this.login.setToken(res.access_token)
-          this.router.navigate([RoutesEnum.Home])
-        }
-      })
+      .subscribe(
+        // Implement next and error
+        res => {
+          if (res.error) {
+            this._snackbar.open('La contraseña o el email son incorrectos', 'Ok', {
+              verticalPosition: 'top',
+              horizontalPosition: 'right'
+            })
+          } else { // No error
+            localStorage.setItem('id', res.id)
+            this.login.setRole(res.role);
+            this.login.setToken(res.access_token)
+            this.router.navigate([RoutesEnum.Hours])
+          }
+        })
   }
 }
