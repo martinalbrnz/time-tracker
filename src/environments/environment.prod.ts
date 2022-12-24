371b39cd52613d75aa7ab91051cb6f6d3e434578
martinalbrnz <martinalbor96@gmail.com>
2022-12-24 02:37:01 -0300

    Fix environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,3 @@
+export const environment = {
+	NG_API: 'https://time-tracker-api-mdb-production.up.railway.app/'
+}
--- src/environments/environments.prod.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-	NG_API: 'http://localhost:3000'
-}
